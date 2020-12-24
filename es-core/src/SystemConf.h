263407bbc8200d221994dc041d5579ccd4fd5506
Christian_Haitian <none@none.com>
2020-12-24 01:16:21 -0500

    Fix compilation due to missing files.

---
+++ es-core/src/SystemConf.h
@@ -0,0 +1,31 @@
+#ifndef EMULATIONSTATION_ALL_SYSTEMCONF_H
+#define EMULATIONSTATION_ALL_SYSTEMCONF_H
+
+
+#include <string>
+#include <map>
+
+class SystemConf 
+{
+public:
+	static SystemConf* getInstance();
+	
+    bool loadSystemConf();
+    bool saveSystemConf();
+
+    std::string get(const std::string &name);
+    bool set(const std::string &name, const std::string &value);
+
+	bool getBool(const std::string &name, bool defaultValue = false);
+	bool setBool(const std::string &name, bool value);
+
+private:
+	SystemConf();
+	static SystemConf* sInstance;
+
+	std::map<std::string, std::string> confMap;
+	bool mWasChanged;
+};
+
+
+#endif //EMULATIONSTATION_ALL_SYSTEMCONF_H
