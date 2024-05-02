6dbedcc5fdc3bacca732f8f78d9d53fdc5442da8
Christian_Haitian <mckman@comcast.net>
2024-05-02 20:04:15 +0000

    Add ability to change launchscreen image and mode to ES

--- es-core/src/Settings.cpp
+++ es-core/src/Settings.cpp
@@ -106,6 +106,8 @@ void Settings::setDefaults()
 	mStringMap["VerbalBatteryWarning"] = "on";
 	mStringMap["GlobalPerformanceGovernor"] = "performance";
 	mStringMap["AutoSuspendTimeout"] = "Off";
+	mStringMap["GameLoadingImage"] = "default";
+	mStringMap["GameLoadingIMode"] = "pic";
 	mBoolMap["OptimizeVRAM"] = true;	
 	mBoolMap["ThreadedLoading"] = true;	
 	mBoolMap["MusicTitles"] = true;
