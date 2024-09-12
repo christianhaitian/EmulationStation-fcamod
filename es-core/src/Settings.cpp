d0da2b0587be28b4b9d7215a8082fa5af1414734
Christian_Haitian <mckman@comcast.net>
2024-09-12 18:15:10 +0000

    Add ability to change image loading delay when launching a game

--- es-core/src/Settings.cpp
+++ es-core/src/Settings.cpp
@@ -108,6 +108,7 @@ void Settings::setDefaults()
 	mStringMap["AutoSuspendTimeout"] = "Off";
 	mStringMap["GameLoadingImage"] = "default";
 	mStringMap["GameLoadingIMode"] = "pic";
+	mStringMap["ImagedelayTime"] = "1.5";
 	mBoolMap["SaveGamelistsOnExit"] = true;
 	mBoolMap["OptimizeVRAM"] = true;	
 	mBoolMap["ThreadedLoading"] = true;	
